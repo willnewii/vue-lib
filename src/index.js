c4121c6d27854761dc4fdc739d8783c63658db0d
诗人的咸鱼 <willnewii@163.com>
2018-08-17 12:48:13 +0800

    fix1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import autoListView from './components/AutoListView';
+// import autoListView from './components/AutoListView';
 
-import MuRefreshControl from "muse-ui/src/refreshControl/refreshControl.vue";
+/*import MuRefreshControl from "muse-ui/src/refreshControl/refreshControl.vue";
 import MuInfiniteScroll from "muse-ui/src/infiniteScroll/infiniteScroll.vue";
 import MuList from "muse-ui/src/list/list.vue";
 import MuListItem from "muse-ui/src/list/listItem.vue";
-import MuDivider from "muse-ui/src/divider/divider.vue";
+import MuDivider from "muse-ui/src/divider/divider.vue";*/
 
 const name = 'vuelib';
 
@@ -12,13 +12,13 @@ const install = function (Vue, options = {}) {
     Vue.prototype['$' + name] = options;
 
     //列表组件
-    Vue.component(MuRefreshControl.name, MuRefreshControl);
+/*    Vue.component(MuRefreshControl.name, MuRefreshControl);
     Vue.component(MuList.name, MuList);
     Vue.component(MuListItem.name, MuListItem);
     Vue.component(MuInfiniteScroll.name, MuInfiniteScroll);
-    Vue.component(MuDivider.name, MuDivider);
+    Vue.component(MuDivider.name, MuDivider);*/
 
-    Vue.component(autoListView.name, autoListView);
+    // Vue.component(autoListView.name, autoListView);
 };
 
 export {name, install};
